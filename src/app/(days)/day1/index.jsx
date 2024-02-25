250c0f829a1217bd857c1ab647ffd30a10a9503c
Moro <njiemoro2@gmail.com>
2024-02-25 14:03:52 +0000

    day 1

--- src/app/(days)/day1/index.jsx
+++ src/app/(days)/day1/index.jsx
@@ -4,9 +4,10 @@ import { Stack } from 'expo-router'
 
 export default function index() {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Stack.Screen options={{title: "day 1"}}  />
       <Text>day 1</Text>
+      <Text>Did some small expo demo</Text>
     </View>
   )
 }

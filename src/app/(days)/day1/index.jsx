86977b112d747c96c2378ee97bd011c701eace0e
Moro <njiemoro2@gmail.com>
2024-02-25 13:46:22 +0000

    expo stack

--- src/app/(days)/day1/index.jsx
+++ src/app/(days)/day1/index.jsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
 
 export default function index() {
   return (
     <View>
+      <Stack.Screen options={{title: "day 1"}}  />
       <Text>day 1</Text>
     </View>
   )
--- src/app/(days)/day2/index.jsx
+++ src/app/(days)/day2/index.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 export default function index() {
   return (
     <View>
+      <Stack.Screen options={{title: "day 1"}}  />
       <Text>day 2</Text>
     </View>
   )
